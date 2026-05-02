9a83a5c849b1e84690f6f5bc5476116d1c1c6776
KoujiKaeru <thelaziestdev05@gmail.com>
2026-05-02 13:15:31 -0700

    Updated alu.v with minor fixes

--- modules/alu.v
+++ modules/alu.v
@@ -1,24 +1,28 @@
+`timescale 1ns/1ps
 /*
 Minimal 8-bit ALU
 */
+
 
 module alu(
 	input wire [7:0] A, 		// Operand A
 	input wire [7:0] B,			// Operand B
 	input wire [7:0] op,		// Op code
-	output reg [7:0] result,
+	output reg [7:0] Y,
 	output reg Z,				// Zero flag
 	output reg C 				// Carry flag
 );
 
-	localparam SHR = 4'h01,
-		SHL = 4'h02,
-		ADD = 4'h30,
-		NAND = 4'h40;
+	//Implied Op
+	localparam SHR  = 4'h1;
+	localparam SHL  = 4'h2;
+	//Op
+	localparam ADD  = 4'h3;
+	localparam NAND = 4'h4;
 
 	always @(*) begin
 		
-		if (op[7:4] == 8'b0000) begin
+		if (op[7:4] == 4'b0000) begin
 			case(op[3:0])
 				SHL: begin 
 						Y = A << 1;
